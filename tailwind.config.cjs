7bec56a923168bbf0bb4fbf9b8f500c4a570561c
svenahac <ahacsh@gmail.com>
2022-07-18 16:06:39 +0200

    front page

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#31427E",
+      secondary: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
